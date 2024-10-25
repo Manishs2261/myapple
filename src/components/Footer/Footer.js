0431a0072f881f04c8b28bf7fd3a7889cf6e8f0f
Manish sahu <sanjusahu7326@gmail.com>
2024-10-25 11:58:15 +0530

    Footer desgin complete

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,17 +5,17 @@ import { MdOutlineShoppingBag } from "react-icons/md";
 
 const Footer = () => {
   return (
-    <div className='flex space-x-5  justify-centers items-center '>
+    <div className='flex space-x-5  justify-center items-center p-3  '>
       <FaApple/>
-       <h3>Store</h3>
-      <h3>Mac</h3>
-      <h3>iPad</h3>
-      <h3>iPhone</h3>
-      <h3>Watch</h3>
-      <h3>TV & Home</h3>
-      <h3>Entertainment</h3>
-      <h3>Accessories</h3>
-      <h3>Support</h3>
+       <h3 className='text-gray-500 hover:text-black'>Store</h3>
+      <h3 className='text-gray-500 hover:text-black'>Mac</h3>
+      <h3 className='text-gray-500 hover:text-black'>iPad</h3>
+      <h3 className='text-gray-500 hover:text-black'>iPhone</h3>
+      <h3 className='text-gray-500 hover:text-black'>Watch</h3>
+      <h3 className='text-gray-500 hover:text-black'>TV & Home</h3>
+      <h3 className='text-gray-500 hover:text-black'>Entertainment</h3>
+      <h3 className='text-gray-500 hover:text-black'>Accessories</h3>
+      <h3 className='text-gray-500 hover:text-black'>Support</h3>
       <CiSearch/>
       <MdOutlineShoppingBag/>
     
