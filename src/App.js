a4000014ca1558fca39a6c399e943104c326b2f7
Manish sahu <sanjusahu7326@gmail.com>
2024-10-26 13:24:38 +0530

    add hero component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Footer from './components/Footer/Footer';
+import Hero from './components/Hero/Hero';
 
 function App() {
   return (
     <>
     <Footer/>
-      
+      <Hero/>
     </>
   );
 }
