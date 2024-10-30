eb5e96a2a414a61cc4c22e593756b6c2067e4c17
Manish sahu <sanjusahu7326@gmail.com>
2024-10-30 12:32:00 +0530

    create significant component

---
+++ src/components/Significant/Significant.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Significant = () => {
+  return (
+    <div>
+      <h1 className="text-5xl font-medium">Significant others.</h1>
+      <div className="w-full h-[1000px] bg-slate-500"></div>
+    </div>
+  )
+}
+
+export default Significant
