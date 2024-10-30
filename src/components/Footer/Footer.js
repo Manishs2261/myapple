eaff0567d7f2d2400dbd1cf20d62702cafa10bd8
Manish sahu <sanjusahu7326@gmail.com>
2024-10-30 12:30:02 +0530

    create footer component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,25 +1,49 @@
 import React from 'react';
-import { FaApple } from "react-icons/fa6";
-import { CiSearch } from "react-icons/ci";
-import { MdOutlineShoppingBag } from "react-icons/md";
+ 
 
 const Footer = () => {
   return (
-    <div className='flex space-x-5  justify-center items-center p-3  '>
-      <FaApple/>
-       <h3 className='text-gray-500 hover:text-black'>Store</h3>
-      <h3 className='text-gray-500 hover:text-black'>Mac</h3>
-      <h3 className='text-gray-500 hover:text-black'>iPad</h3>
-      <h3 className='text-gray-500 hover:text-black'>iPhone</h3>
-      <h3 className='text-gray-500 hover:text-black'>Watch</h3>
-      <h3 className='text-gray-500 hover:text-black'>TV & Home</h3>
-      <h3 className='text-gray-500 hover:text-black'>Entertainment</h3>
-      <h3 className='text-gray-500 hover:text-black'>Accessories</h3>
-      <h3 className='text-gray-500 hover:text-black'>Support</h3>
-      <CiSearch/>
-      <MdOutlineShoppingBag/>
-    
+    <div className="bg-slate-50 w-full h-[700px]">
+    <h1 className="text-5xl font-medium mt-24 px-20 pt-28">Mac</h1>
+    <div className="flex px-20 mt-10 space-x-16">
+      <div className="space-y-2">
+        <h5 className=" text-slate-400">Explore Mac</h5>
+
+        <h2 className="text-xl font-medium">Explore All Mac</h2>
+        <h2 className="text-xl font-medium">MacBook Air</h2>
+        <h2 className="text-xl font-medium">MacBook Pro</h2>
+        <h2 className="text-xl font-medium">iMac</h2>
+        <h2 className="text-xl font-medium">Mac mini</h2>
+        <h2 className="text-xl font-medium">Mac Studio</h2>
+        <h2 className="text-xl font-medium">Mac Pro</h2>
+        <h2 className="text-xl font-medium">Display</h2>
+
+        <h5 className="  text-xs  font-medium">Compare Mac</h5>
+        <h5 className="text-xs font-medium">Switch from PC to Mac</h5>
+      </div>
+
+      <div>
+        <h5 className="text-slate-400 ">Shop Mac</h5>
+
+        <h3 className="font-medium">Shop Mac</h3>
+        <h3 className="font-medium">Mac Accessories</h3>
+        <h3 className="font-medium">Ways to Buy</h3>
+      </div>
+
+      <div>
+        <h5 className="text-slate-400 ">More from Mac</h5>
+        <h2 className="font-medium">Mac Support</h2>
+        <h2 className="font-medium">AppleCare+ for Mac</h2>
+        <h2 className="font-medium">macOS Sequoia</h2>
+        <h2 className="font-medium">Apple Intelligence</h2>
+        <h2 className="font-medium">Apps by Apple</h2>
+        <h2 className="font-medium">Continuity</h2>
+        <h2 className="font-medium">iCloud+</h2>
+        <h2 className="font-medium">Mac for Business</h2>
+        <h2 className="font-medium">Education</h2>
+      </div>
     </div>
+  </div>
   )
 }
 
