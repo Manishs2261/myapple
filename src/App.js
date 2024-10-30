c66c35b9e1b0ffba517fa915cede1d0e9a43b0d3
Manish sahu <sanjusahu7326@gmail.com>
2024-10-30 12:33:02 +0530

    manage header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
 import Hero from './components/Hero/Hero';
 
 function App() {
   return (
     <>
-    <Footer/>
+    <Header/>
       <Hero/>
+      <Footer/>
     </>
   );
 }
