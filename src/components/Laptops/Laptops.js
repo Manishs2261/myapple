82deeedd97f34b8b9747bc04ce54066dedd56bdd
Manish sahu <sanjusahu7326@gmail.com>
2024-10-30 12:31:16 +0530

    create laptop component

---
+++ src/components/Laptops/Laptops.js
@@ -0,0 +1,173 @@
+import React from 'react'
+import air13 from "../../assets/air13.png";
+import { IoIosArrowForward } from "react-icons/io";
+import { FaApple } from "react-icons/fa";
+
+const Laptops = () => {
+  return (
+    <div>
+          <div className="bg-white max-w-fit">
+                  <div className="flex">
+                    <div className="items-center  max-w-fit p-4 justify-center flex-col text-center">
+                      <img
+                        className=""
+                        width={250}
+                        src={air13}
+                        alt="appleimage"
+                      />
+                      <h5 className="text-red-600 text-xs mt-4">
+                        New M3 models
+                      </h5>
+                      <h1 className="text-xl font-medium mt-2">
+                        MacBook Air 13" and 15"
+                      </h1>
+                      <h4 className="font-medium">M2 or M3 chip</h4>
+                      <h3 className="text-sm mt-2">
+                        Strikingly thin and fast so you can{" "}
+                      </h3>
+                      <h3 className="text-sm">work, play or create anywhere</h3>
+                      <h3 className="font-medium mt-2">From $999</h3>
+                      <div className="flex justify-center items-center mt-4">
+                        <button className="bg-blue-500 py-1 px-4 rounded-full text-white mr-8">
+                          Learn more
+                        </button>
+
+                        <div className="flex items-end ">
+                          <h5 className=" text-blue-600 ">buy</h5>{" "}
+                          {/* <div className="text-blue-600"> */}
+                          <IoIosArrowForward
+                            className="text-blue-600 items-end justify-items-end"
+                            size={10}
+                          />
+                          {/* </div> */}
+                        </div>
+                      </div>
+                    </div>
+
+                    <div className="items-center  max-w-fit p-4 justify-center flex-col text-center">
+                      <img
+                        className=""
+                        width={250}
+                        src={air13}
+                        alt="appleimage"
+                      />
+                      <h5 className="text-red-600 text-xs mt-4">
+                        New M3 models
+                      </h5>
+                      <h1 className="text-xl font-medium mt-2">
+                        MacBook Air 13" and 15"
+                      </h1>
+                      <h4 className="font-medium">M2 or M3 chip</h4>
+                      <h3 className="text-sm mt-2">
+                        Strikingly thin and fast so you can{" "}
+                      </h3>
+                      <h3 className="text-sm">work, play or create anywhere</h3>
+                      <h3 className="font-medium mt-2">From $999</h3>
+                      <div className="flex justify-center items-center mt-4">
+                        <button className="bg-blue-500 py-1 px-4 rounded-full text-white mr-8">
+                          Learn more
+                        </button>
+
+                        <div className="flex items-end ">
+                          <h5 className=" text-blue-600 ">buy</h5>{" "}
+                          {/* <div className="text-blue-600"> */}
+                          <IoIosArrowForward
+                            className="text-blue-600 items-end justify-items-end"
+                            size={10}
+                          />
+                          {/* </div> */}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="flex h-0.5 bg-gray-500 my-8"></div>
+
+                  <div className="flex">
+                    <div className="  p-7 items-center flex-col max-w-fit text-center">
+                      <h1 className="text-2xl font-medium py-2">
+                        34.46 cm or 38.91 cm
+                      </h1>
+                      <h6 className="text-xs">
+                        (13.6″ or 15.3″) Liquid Retina display with
+                      </h6>
+                      <h6 className="text-xs">
+                        500 nits of brightness and support for
+                      </h6>
+                      <h6 className="text-xs">1 billion colours5</h6>
+
+                      <div className="flex justify-center items-center mt-8 mb-2 space-x-2">
+                        <div className="max-w-fit p-1 bg-black justify-center items-center flex rounded-md">
+                          <FaApple className="text-white" />
+                          <h2 className="text-white">M2</h2>
+                        </div>
+                        <div className="max-w-fit p-1 bg-black justify-center items-center flex rounded-md">
+                          <FaApple className="text-white" />
+                          <h2 className="text-white">M3</h2>
+                        </div>
+                      </div>
+                      <h5 className="text-xs mb-8">Apple M2 or M3 chip</h5>
+
+                      <h5 className="text-xs ">Up to</h5>
+                      <h1 className="text-xl font-medium">18 hours</h1>
+                      <h5 className="text-xs mb-8">battery life6</h5>
+
+                      <h1 className="text-xl font-medium">4 ports</h1>
+                      <h5 className="text-xs">2x Thunderbolt / USB 4,</h5>
+                      <h5 className="text-xs mb-8">headphone jack, MagSafe</h5>
+
+                      <h1 className="text-xl font-medium ">
+                        1.24 kg or 1.51 kg
+                      </h1>
+                      <h1 className="text-xl font-medium ">
+                        (2.7 lb. or 3.3 lb.)
+                      </h1>
+                      <h5 className="text-xs">Weight</h5>
+                    </div>
+                    <div className="  p-7 items-center flex-col max-w-fit text-center">
+                      <h1 className="text-2xl font-medium py-2">
+                        34.46 cm or 38.91 cm
+                      </h1>
+                      <h6 className="text-xs">
+                        (13.6″ or 15.3″) Liquid Retina display with
+                      </h6>
+                      <h6 className="text-xs">
+                        500 nits of brightness and support for
+                      </h6>
+                      <h6 className="text-xs">1 billion colours5</h6>
+
+                      <div className="flex justify-center items-center mt-8 mb-2 space-x-2">
+                        <div className="max-w-fit p-1 bg-black justify-center items-center flex rounded-md">
+                          <FaApple className="text-white" />
+                          <h2 className="text-white">M2</h2>
+                        </div>
+                        <div className="max-w-fit p-1 bg-black justify-center items-center flex rounded-md">
+                          <FaApple className="text-white" />
+                          <h2 className="text-white">M3</h2>
+                        </div>
+                      </div>
+                      <h5 className="text-xs mb-8">Apple M2 or M3 chip</h5>
+
+                      <h5 className="text-xs ">Up to</h5>
+                      <h1 className="text-xl font-medium">18 hours</h1>
+                      <h5 className="text-xs mb-8">battery life6</h5>
+
+                      <h1 className="text-xl font-medium">4 ports</h1>
+                      <h5 className="text-xs">2x Thunderbolt / USB 4,</h5>
+                      <h5 className="text-xs mb-8">headphone jack, MagSafe</h5>
+
+                      <h1 className="text-xl font-medium ">
+                        1.24 kg or 1.51 kg
+                      </h1>
+                      <h1 className="text-xl font-medium ">
+                        (2.7 lb. or 3.3 lb.)
+                      </h1>
+                      <h5 className="text-xs">Weight</h5>
+                    </div>
+                  </div>
+                </div>
+    </div>
+  )
+}
+
+export default Laptops
